4e695a73016a03cdedff7351485b8c0d885960e4
aarunsrinivas <aarunsrinivas@gmail.com>
2022-11-12 17:58:56 -0800

    format

--- canny/canny.py
+++ canny/canny.py
@@ -2,67 +2,73 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def canny_edge_detector(img_path):
-    # read image and convert to grayscale
-    img = cv2.imread(img_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    # reduce noise
-    img = cv2.GaussianBlur(img, (5, 5), 1.4)
+class CannyEdgeDetector:
 
-    # calculate gradients in the x and y direction
-    Ix = cv2.Sobel(np.float32(img), cv2.CV_64F, 1, 0, 3)
-    Iy = cv2.Sobel(np.float32(img), cv2.CV_64F, 0, 1, 3)
+    def __init__(self, weak_threshold=None, strong_threshold=None):
+        self.weak_threshold = weak_threshold
+        self.strong_threshold = strong_threshold
 
-    # calculate magnitude and direction of gradients
-    G, theta = cv2.cartToPolar(Ix, Iy, angleInDegrees=True)
+    def __call__(self, img_path):
+        img = self.read_image(img_path)
+        img = self.reduce_noise(img)
+        Ix, Iy = self.calculate_gradients(img)
+        G, theta = self.calculate_magnitude_and_direction(Ix, Iy)
+        G_suppress = self.non_max_suppression(img, G, theta)
+        G_threshold = self.double_thresholding(G_suppress)
+        return G_threshold
 
-    # eliminates vectors that are opposite to vectors with theta 0-180
-    theta[theta > 180] -= 180
+    def read_image(self, img_path):
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        return img
 
-    M, N = img.shape
+    def reduce_noise(self, img):
+        img = cv2.GaussianBlur(img, (5, 5), 1.4)
+        return img
 
-    # non-max suppression algorithm
-    for i in range(1, M - 1):
-        for j in range(1, N - 1):
+    def calculate_gradients(self, img):
+        Ix = cv2.Sobel(np.float32(img), cv2.CV_64F, 1, 0, 3)
+        Iy = cv2.Sobel(np.float32(img), cv2.CV_64F, 0, 1, 3)
+        return Ix, Iy
 
-            # find neighbors along gradient angle for each pixel
-            n1, n2 = None, None
-            # covers x-axis
-            if (0 <= theta[i, j] < 22.5) or (157.5 <= theta[i, j] <= 180):
-                n1, n2 = G[i, j - 1], G[i, j + 1]
-            # covers Q1-Q3 diagonal
-            elif 22.5 <= theta[i, j] < 67.5:
-                n1, n2 = G[i + 1, j - 1], G[i - 1, j + 1]
-            # covers y-axis
-            elif 67.5 <= theta[i, j] < 112.5:
-                n1, n2 = G[i + 1, j], G[i - 1, j]
-            # covers Q2-Q4 diagonal
-            elif 112.5 <= theta[i, j] < 157.5:
-                n1, n2 = G[i + 1, j + 1], G[i - 1, j - 1]
-            
-            # non-max suppression step
-            if G[i, j] < n1 or G[i, j] < n2:
-                G[i, j] = 0
+    def calculate_magnitude_and_direction(self, Ix, Iy):
+        G, theta = cv2.cartToPolar(Ix, Iy, angleInDegrees=True)
+        theta[theta > 180] -= 180 # remove opposite facing vectors
+        return G, theta
 
+    def non_max_suppression(self, img, G, theta):
+        G = G.copy()
+        M, N = img.shape
+        for i in range(1, M - 1):
+            for j in range(1, N - 1):
+                # find neighbors along gradient angle for each pixel
+                n1, n2 = None, None
+                # covers x-axis
+                if (0 <= theta[i, j] < 22.5) or (157.5 <= theta[i, j] <= 180):
+                    n1, n2 = G[i, j - 1], G[i, j + 1]
+                # covers Q1-Q3 diagonal
+                elif 22.5 <= theta[i, j] < 67.5:
+                    n1, n2 = G[i + 1, j - 1], G[i - 1, j + 1]
+                # covers y-axis
+                elif 67.5 <= theta[i, j] < 112.5:
+                    n1, n2 = G[i + 1, j], G[i - 1, j]
+                # covers Q2-Q4 diagonal
+                elif 112.5 <= theta[i, j] < 157.5:
+                    n1, n2 = G[i + 1, j + 1], G[i - 1, j - 1]
+                # non-max suppression step
+                if G[i, j] < n1 or G[i, j] < n2:
+                    G[i, j] = 0
+        return G
 
-    # double thresholding
-    G_max = np.max(G)
-    weak_threshold = G_max * 0.1
-    strong_threshold = G_max * 0.5
-    G[G < weak_threshold] = 0
-    # result = np.zeros_like(img)
+    def double_thresholding(self, G):
+        G = G.copy()
+        G_max = np.max(G)
+        weak_threshold = self.weak_threshold or G_max * 0.1
+        # strong_threshold = self.strong_threshold or G_max * 0.5
+        G[G < weak_threshold] = 0
+        return G
 
-    # strong_coordinates = np.where(Z >= strong_threshold)
-    # weak_coordinates = np.where((Z >= weak_threshold) & (Z < strong_threshold))
-
-    # result[strong_coordinates] = 255
-    # result[weak_coordinates] = 25
-
-    #TODO need to add hysteris step
-
-    return G
-    
+canny_edge_detector = CannyEdgeDetector()
 canny_img = canny_edge_detector('mountain.png')
 plt.imshow(canny_img)
 plt.show()
--- hog/hog.py
+++ hog/hog.py
@@ -1,49 +1,64 @@
 import cv2
 import numpy as np
 
-def hog_feature_descriptor(img_path):
+class HOG:
 
-    # convert image to size 64x128
-    img = cv2.imread(img_path)
-    img = cv2.resize(img, (64, 128))
+    def __call__(self, img_path):
+        img = self.read_image(img_path)
+        Ix, Iy = self.calculate_gradients(img)
+        G, theta = self.calculate_magnitude_and_direction(Ix, Iy)
+        G_max, theta_max = self.find_maximum_grad_along_color_channels(G, theta)
+        feature_descriptor = self.compute_feature_descriptor(G_max, theta_max)
+        return feature_descriptor
 
-    # calculate gradients in the x and y direction
-    Ix = cv2.Sobel(np.float32(img), cv2.CV_64F, 1, 0, 3)
-    Iy = cv2.Sobel(np.float32(img), cv2.CV_64F, 0, 1, 3)
+    def read_image(self, img_path):
+        # convert image to size 64x128
+        img = cv2.imread(img_path)
+        img = cv2.resize(img, (64, 128))
+        return img
 
-    # calculate magnitude and direction of gradients
-    G, theta = cv2.cartToPolar(Ix, Iy, angleInDegrees=True)
-    
-    idx = np.argmax(G, axis=2)
-    G_max = np.zeros((128, 64))
-    theta_max = np.zeros((128, 64))
-    for i in range(128):
-        for j in range(64):
-            G_max[i, j] = G[i, j, idx[i, j]]
-            theta_max[i, j] = theta[i, j, idx[i, j]]
+    def calculate_gradients(self, img):
+        Ix = cv2.Sobel(np.float32(img), cv2.CV_64F, 1, 0, 3)
+        Iy = cv2.Sobel(np.float32(img), cv2.CV_64F, 0, 1, 3)
+        return Ix, Iy
 
-    theta_max[theta_max >= 180] -= 180
+    def calculate_magnitude_and_direction(self, Ix, Iy):
+        G, theta = cv2.cartToPolar(Ix, Iy, angleInDegrees=True)
+        return G, theta
 
-    image_vector = np.array([])
-    for i in range(15):
-        for j in range(7):
-            G_window = G_max[i:i + 16, j:j + 16]
-            theta_window = theta_max[i:i + 16, j:j + 16]
-            window_vector = np.array([])
-            for k in range(2):
-                for l in range(2):
-                    block_vector = np.zeros((9,))
-                    G_block = G_window[k * 8:(k + 1) * 8, l * 8:(l + 1) * 8].reshape(-1)
-                    theta_block = theta_window[k * 8:(k + 1) * 8, l * 8:(l + 1) * 8].reshape(-1)
-                    for m in range(len(G_block)):
-                        x = int(np.floor(theta_block[m] / 20)) % 9
-                        y = (x + 1) % 9
-                        block_vector[x] += abs(theta_block[m] - x * 20) / 20
-                        block_vector[y] += abs(theta_block[m] - y * 20) / 20
-                    window_vector = np.concatenate([window_vector, block_vector])
-            window_vector = window_vector / np.linalg.norm(window_vector)
-            image_vector = np.concatenate([image_vector, window_vector])
-    
-    return image_vector
+    def find_maximum_grad_along_color_channels(self, G, theta):
+        idx = np.argmax(G, axis=2)
+        G_max = np.zeros((128, 64))
+        theta_max = np.zeros((128, 64))
+        for i in range(128):
+            for j in range(64):
+                G_max[i, j] = G[i, j, idx[i, j]]
+                theta_max[i, j] = theta[i, j, idx[i, j]]
+        theta_max[theta_max >= 180] -= 180
+        return G_max, theta_max
 
-hog_feature = hog_feature_descriptor('puppy.png')
+    def compute_feature_descriptor(self, G_max, theta_max):
+        feature_descriptor = np.array([])
+        # sweep 16 x 16 window one pixel at a time across image
+        for i in range(15):
+            for j in range(7):
+                G_window = G_max[i:i + 16, j:j + 16]
+                theta_window = theta_max[i:i + 16, j:j + 16]
+                window_descriptor = np.array([])
+                for k in range(2):
+                    for l in range(2):
+                        block_descriptor = np.zeros((9,))
+                        G_block = G_window[k * 8:(k + 1) * 8, l * 8:(l + 1) * 8].reshape(-1)
+                        theta_block = theta_window[k * 8:(k + 1) * 8, l * 8:(l + 1) * 8].reshape(-1)
+                        for m in range(len(G_block)):
+                            x = int(np.floor(theta_block[m] / 20)) % 9
+                            y = (x + 1) % 9
+                            block_descriptor[x] += abs(theta_block[m] - x * 20) / 20
+                            block_descriptor[y] += abs(theta_block[m] - y * 20) / 20
+                        window_descriptor = np.concatenate([window_descriptor, block_descriptor])
+                window_descriptor = window_descriptor / np.linalg.norm(window_descriptor)
+                feature_descriptor = np.concatenate([feature_descriptor, window_descriptor])
+        return feature_descriptor
+
+hog = HOG()
+hog_feature_descriptor = hog('puppy.png')
